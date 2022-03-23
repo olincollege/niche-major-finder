521351b617759c8a2c02035835f8b1c0e9189960
Jiayuan Liu <jliu2@olin.edu>
2022-03-22 21:57:54 -0400

    Add a list of state names and new way to find total number of colleges.

--- web_scraping/niche_web_scraping.py
+++ web_scraping/niche_web_scraping.py
@@ -1,10 +1,44 @@
+from types import AsyncGeneratorType
 import requests
 import math
 import pandas as pd
 
 from bs4 import BeautifulSoup as BS
 
-agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+# code that scrapts a web page for state names...
+state_url = 'https://alphabetizer.flap.tv/lists/list-of-states-in-alphabetical-order.php'
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14;\
+    rv:66.0) Gecko/20100101 Firefox/66.0", "Accept":\
+    "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",\
+    "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": \
+    "gzip, deflate", "DNT": "1", "Connection": "close", \
+    "Upgrade-Insecure-Requests": "1"}
+
+def getHTMLdocument(url):
+      
+    # request for HTML document of given url
+    
+    response = requests.get(url, headers=headers)
+      
+    # response will be provided in JSON format
+    return response.text
+
+html_document = getHTMLdocument(state_url)
+soup = BS(html_document, 'html.parser')
+data_states = soup.find("div", class_="panel-body")
+states_uppercase = str(data_states.ul.get_text())
+states = states_uppercase.lower()
+state_list = states.split("\n")
+
+# Rucha's agent
+#agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+# Jiayuan's agent (changing agent does not seem to do anything)
+agent = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14;\
+    rv:66.0) Gecko/20100101 Firefox/66.0", "Accept":\
+    "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",\
+    "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": \
+    "gzip, deflate", "DNT": "1", "Connection": "close", \
+    "Upgrade-Insecure-Requests": "1"}
 
 # Create pandas dataframe
 df = pd.DataFrame()
@@ -20,6 +54,10 @@ for i in states_list:
     # Get the total number of colleges in the state from the text
     #number_of_colleges = html.select('.search-result-counter')[0].get_text()
     #number_of_colleges = int(number_of_colleges[:-8])
+    
+    # Another method that gets the total number of colleges
+    #number_of_colleges = int(html.find("div", class_="search-result-counter").get_text().split("\xa0")[0])
+
 
     # Find the number of colleges in the top 10% for each state
     #top_ten_percent_of_colleges = math.ceil(0.1 * number_of_colleges)
