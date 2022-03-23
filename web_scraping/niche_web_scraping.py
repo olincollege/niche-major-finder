105cd983d23c370116346ea8fc05084ef272e1be
Rucha Dave <ruchadave16@gmail.com>
2022-03-22 21:34:23 -0400

    Worked on creating the dataframe by looping through each college

--- web_scraping/niche_web_scraping.py
+++ web_scraping/niche_web_scraping.py
@@ -1,33 +1,51 @@
 import requests
 import math
+import pandas as pd
 
 from bs4 import BeautifulSoup as BS
 
-
 agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
 
-states = ["alabama"]
+# Create pandas dataframe
+df = pd.DataFrame()
 
-for i in states:
+states_list = ["alabama"]
+
+for i in states_list:
+    # Find text from the college listings for each state
     url = f"https://www.niche.com/colleges/search/all-colleges/s/{i}/?type=private&type=communityCollege&type=public"
     page = requests.get(url, headers=agent)
-    #print (BS(page.content, 'lxml'))
     html = BS(page.text, "html.parser")
-    colleges = html.select("ol.search-results__list")
-    #print(colleges)
+    print(html)
+    # Get the total number of colleges in the state from the text
+    #number_of_colleges = html.select('.search-result-counter')[0].get_text()
+    #number_of_colleges = int(number_of_colleges[:-8])
 
-    # Get the number of colleges in a state from the number of results produced
-    number_of_colleges = html.select(".search-result-counter")[0].get_text()
-    number_of_colleges = int(number_of_colleges[:-8])
+    # Find the number of colleges in the top 10% for each state
+    #top_ten_percent_of_colleges = math.ceil(0.1 * number_of_colleges)
+    top_ten_percent_of_colleges = 10
 
-    # Find the number of colleges in the top 10%
-    top_ten_percent_of_colleges = math.ceil(0.1 * number_of_colleges)
+    # Loop through all colleges on first page while total < 10% number
+    colleges = html.find_all(attrs={'class':"search-result"})
+    total_colleges_yet = 0
 
-    # Get the information on the colleges in the top 10%
-    # Name, number of ppl per major, total students
-    # *Convert to pandas dataframe
-    
-    for college in colleges:
-        college_title = college.select('.search-result__title')[0].get_text()
-        print(colleges)
-        print(college_title)
+    while total_colleges_yet < top_ten_percent_of_colleges:
+        college = colleges[total_colleges_yet]
+        college_name = college["aria-label"].replace(" ", "-")
+
+        # Access each college website
+        college_url = f"https://www.niche.com/colleges/{college_name}/"
+        college_page = requests.get(college_url, headers=agent)
+        college_html = BS(college_page.text, "html.parser")
+
+        # Loop through information for each major on the website and add to df
+        top_majors = college_html.find_all(attrs={'class':"popular-entity"})
+
+        for major in top_majors:
+            df["Major"] = major.select('.popular-entity__name')[0].get_text()
+            df["Students"] = major.select('.popular-entity-descriptor')[0].get_text()
+            df["State"] = i
+            df["Name"] = college["aria-label"]
+
+        total_colleges_yet += 1
+    print(df)
