b589689cc269eb9b8ac074e919db61aa6ac69453
Rucha Dave <ruchadave16@gmail.com>
2022-03-26 22:10:13 -0400

    finished new york

--- web_scraping/niche_web_scraping.py
+++ web_scraping/niche_web_scraping.py
@@ -18,15 +18,14 @@ def get_html_for_url(url):
 agent = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
 
 # Find top 10% of colleges for each state
-states_list = ["new-york"]
-#["north-carolina", "north-dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode-island", "south-carolina", "south-dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west-virginia", "wisconsin", "wyoming"]
+states_list = ["north-carolina", "north-dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode-island", "south-carolina", "south-dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west-virginia", "wisconsin", "wyoming"]
 
 for i in states_list:
     data = []
 
     # Find text from the college listings for each state
     #html = get_html_for_url(f"http://webcache.googleusercontent.com/search?q=cache:https://www.niche.com/colleges/search/all-colleges/s/{i}/?page=2/")
-    html = get_html_for_url(f"https://www.niche.com/colleges/search/all-colleges/s/{i}/?page=2")
+    html = get_html_for_url(f"https://www.niche.com/colleges/search/all-colleges/s/{i}/")
 
     # Get the total number of colleges in the state from the text
     number_of_colleges = html.select('.search-result-counter')[0].get_text()
@@ -39,10 +38,10 @@ for i in states_list:
     # Loop through all colleges on first page while total < 10% number
     colleges = html.find_all(attrs={'class':"search-result"})
     total_colleges_yet = 0
-    index_on_page = 8
-    current_page = 2
+    index_on_page = 0
+    current_page = 1
     
-    while total_colleges_yet < 8:
+    while total_colleges_yet < top_ten_percent_of_colleges:
         # Pause for 20 sec to allow scraping
         time.sleep(20)
 
@@ -86,7 +85,7 @@ for i in states_list:
 
         # Create csv for each state's data
         df = pd.DataFrame(data, columns=["State", "College", "Major", "Students"])
-        df.to_csv(f"{i}Data4.csv", index=False)
+        df.to_csv(f"{i}Data.csv", index=False)
         print(data)
 
         
