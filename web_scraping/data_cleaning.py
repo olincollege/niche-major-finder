5dc681851a8de2aea0249bb8b9b1835c09db62de
Rucha Dave <ruchadave16@gmail.com>
2022-03-28 13:12:51 -0400

    combine csvs into one df

---
+++ web_scraping/data_cleaning.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+import glob
+
+def files_to_df():
+    """
+    This function __FINISH__
+    Args:
+        folder_name:
+
+    Returns:
+
+    """
+    os.chdir("raw_data")
+    all_filenames = sorted([i for i in glob.glob("*.{}".format("csv"))])
+    print(all_filenames)
+    combined_data = pd.concat([pd.read_csv(f) for f in all_filenames])
+    combined_csv = combined_data.to_csv("combined_data.csv", index=False, encoding="utf-8-sig")
+
+    df = pd.read_csv("combined_data.csv")
+    return df
+
+df = files_to_df()
