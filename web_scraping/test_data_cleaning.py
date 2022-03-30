23ece3cbc56074dae49920df9daf13be8ba6fdfb
Rucha Dave <ruchadave16@gmail.com>
2022-03-30 14:23:16 -0400

    continued working on test file

--- web_scraping/test_data_cleaning.py
+++ web_scraping/test_data_cleaning.py
@@ -4,14 +4,56 @@ from collections import Counter
 import pytest
 
 from data_cleaning import (
+    get_key,
+    find_broader_major,
     replace_major_with_broader_major,
-    sum_common_broad_major_for_each_state,
-    sum_common_broad_major_for_all_states,
-    sum_common_broad_major_for_country,
+    sum_broad_major_per_state,
+    sum_broad_major_country,
 )
 
+testing_dict = {'Computer Science': ['Computer Science',\
+    'Computer Software Engineering', 'Information Technology', \
+    'Computer Programming', 'Data Processing', \
+    'Computer and Information Sciences', 'Human Computer Interaction', \
+    'Computer Systems Networking and Telecommunications', \
+    'Network, Database, and System Administration', \
+    'Computer and Information Systems Security', \
+    'Computer Engineering Technician', 'Systems Science and Theory'], 
+    'Science': ['Physical Sciences', 'Physics', 'Engineering Physics', \
+    'Chemistry', 'Apparel and Textile Science', \
+    'Pharmacology and Toxicology', 'Bioinformatics', 'Cellular Biology', \
+    'Biology', 'Biochemistry and Molecular Biology', \
+    'Ecology and Evolutionary Biology', 'Microbiology'], 
+    'Math': ['Mathematics', 'Computational and Applied Mathematics', \
+    'Statistics'], 
+    'Psychology': ['Psychology', 'Physiological Psychology', \
+    'Research and Experimental Psychology', 'Counseling Psychology', \
+    'Psychiatric and Mental Health Services', \
+    'Developmental and Child Psychology', 'Social Psychology', \
+    'Human Development', 'Mental and Social Health Services',\
+    'Community Health Services and Counseling',\
+    'Marriage and Family Therapy and Counseling', 'Cognitive Science'], 
+    'Agriculture and Environment': ['Agricultural Business',\
+    'Animal Sciences and Husbandry', 'Horticulture', 'Agriculture', \
+    'Veterinary Technician and Assistant', 'Equine Studies', \
+    'Plant Science', 'Agricultural Production Operations',\
+    'Crop and Soil Sciences', 'Agricultural Teacher Education', \
+    'Agricultural and Food Products Processing', \
+    'Agricultural Engineering', 'Agricultural Mechanics and Machinery', \
+    'Sustainability Studies', 'Environmental Science', \
+    'Wildlife and Fisheries Management', \
+    'Natural Resources Conservation and Management', \
+    'Zoology and Entomology', 'Marine Science', \
+    'Atmospheric Sciences and Meteorology', 'Geology and Earth Science', \
+    'Geography', 'Marine Biology and Oceanography', 'Natural Sciences', \
+    'Landscaping and Groundskeeping']}
+
 # Define sets of test cases
-replace_major_with_broader_major = [
-    #
-    (), 
+get_key = [
+    # Check for key in the dictionary
+    (, testing_dict), 
 ]
+
+# Define standard testing functions to check functions' outputs given certain
+# inputs defined above.
+@pytest.mark.parametrize("value,testing_dict", get_key)
