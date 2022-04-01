5b6eee2c7e0fd879c4d5bc5d7cbcbaf0602f0b9b
Emma Mascillaro <emascillaro@gmail.com>
2022-03-31 23:07:02 -0400

    clean data_visualization

--- web_scraping/data_visualization.py
+++ web_scraping/data_visualization.py
@@ -128,12 +128,12 @@ def plot_number_of_students_per_major_usa():
     """
 
     plt.barh(MAJORS_LIST, STUDENTS_LIST, align="center", alpha=.5)
-    plt.title('Number of Students Enrolled in Different Majors Offered by the top 10% of Colleges in Each State')
+    plt.title('Number of Students Enrolled in Different Majors Offered by" + \
+        "the top 10% of Colleges in Each State')
     plt.xlabel('College Major')
     plt.ylabel('Number of Students Enrolled')
-    plt.show()
     plt.savefig("number_of_students_per_major_usa.png")
-
+    plt.show()
 
 def plot_major_ratios_in_stacked_bar_graph():
     """
@@ -156,17 +156,17 @@ def plot_major_ratios_in_stacked_bar_graph():
     y_10 = np.array(STUDENTS_IN_A_MAJOR_PER_STATE_ALL_MAJORS[9])
 
     # plot bars in stack manner
-    STATES_LIST = list(BROADER_MAJOR_SUMMED_DATA_DF['State'].unique())
-    plt.barh(STATES_LIST, y_1, color='r')
-    plt.barh(STATES_LIST, y_2, left=y_1)
-    plt.barh(STATES_LIST, y_3, left=y_1+y_2)
-    plt.barh(STATES_LIST, y_4, left=y_1+y_2+y_3)
-    plt.barh(STATES_LIST, y_5, left=y_1+y_2+y_3+y_4)
-    plt.barh(STATES_LIST, y_6, left=y_1+y_2+y_3+y_4+y_5)
-    plt.barh(STATES_LIST, y_7, left=y_1+y_2+y_3+y_4+y_5+y_6)
-    plt.barh(STATES_LIST, y_8, left=y_1+y_2+y_3+y_4+y_5+y_6+y_7)
-    plt.barh(STATES_LIST, y_9, left=y_1+y_2+y_3+y_4+y_5+y_6+y_7+y_8)
-    plt.barh(STATES_LIST, y_10, left=y_1+y_2+y_3+y_4+y_5+y_6+y_7+y_8+y_9)
+    states_list = list(BROADER_MAJOR_SUMMED_DATA_DF['State'].unique())
+    plt.barh(states_list, y_1, color='r')
+    plt.barh(states_list, y_2, left=y_1)
+    plt.barh(states_list, y_3, left=y_1+y_2)
+    plt.barh(states_list, y_4, left=y_1+y_2+y_3)
+    plt.barh(states_list, y_5, left=y_1+y_2+y_3+y_4)
+    plt.barh(states_list, y_6, left=y_1+y_2+y_3+y_4+y_5)
+    plt.barh(states_list, y_7, left=y_1+y_2+y_3+y_4+y_5+y_6)
+    plt.barh(states_list, y_8, left=y_1+y_2+y_3+y_4+y_5+y_6+y_7)
+    plt.barh(states_list, y_9, left=y_1+y_2+y_3+y_4+y_5+y_6+y_7+y_8)
+    plt.barh(states_list, y_10, left=y_1+y_2+y_3+y_4+y_5+y_6+y_7+y_8+y_9)
 
     plt.xlabel("Number of Students Enrolled")
     plt.ylabel("State")
@@ -178,11 +178,10 @@ def plot_major_ratios_in_stacked_bar_graph():
                 MAJORS_BY_DESCENDING_POPULARITY[7], MAJORS_BY_DESCENDING_POPULARITY[8],
                 MAJORS_BY_DESCENDING_POPULARITY[9], MAJORS_BY_DESCENDING_POPULARITY[10]])
 
-    plt.title("Distribution of the Top 10 College Majors in the Country by \
-        State")
-    plt.show()
+    plt.title("Distribution of the Top 10 College Majors in the Country by " +\
+        "State")
     plt.savefig('major_ratios_in_stacked_bar_graph.png')
-
+    plt.show()
 
 def plot_major_distribution_in_usa_pie_chart():
     """
@@ -195,14 +194,15 @@ def plot_major_distribution_in_usa_pie_chart():
     BROADER_MAJOR_WHOLE_COUNTRY_DF["Students"].to_list()
     plt.pie(number_of_students_per_major_usa,
             labels=BROADER_MAJOR_WHOLE_COUNTRY_DF["Major"].to_list(), rotatelabels=True)
-    plt.show()
-    plt.savefig("major_distribution_in_usa_pie_chart.png")
 
+    plt.title('Percentage of Students in each Major Across the USA')
+    plt.savefig("major_distribution_in_usa_pie_chart.png")
+    plt.show()
 
 
 ALL_STUDENTS_PER_STATE = total_students_per_state(BROADER_MAJOR_SUMMED_DATA_DF)
 
-# Read shape file to create a bas USA map
+# Read shape file to create a base USA map
 STATES = geopandas.read_file("usa-states-census-2014.shp")
 
 # Sort the dataframe alphabetically by state names
@@ -218,100 +218,119 @@ RM_STATES_LIST = STATES_LIST
 RM_STATES_LIST.remove("alaska")
 RM_STATES_LIST.remove("hawaii")
 
-def generate_business_density_map(BROADER_MAJOR_SUMMED_DATA_DF, RM_STATES_LIST, STATES):
+def generate_business_density_map(broader_major_summed_data_df, rm_states_list, states):
+    """
+    This function creates a map of the USA, representing the density of
+    business majors in each state.
 
+    Args:
+        broader_major_summed_data_df: A pandas dataframe which represents the
+        number of students in all majors for all states.
+        rm_states_list:  A list of all states in the USA except for Alaska and Hawaii.
+        states: A geopandas dataframe containing the shape data for the USA plot.
+    """
     # Create a list of the number of students majoring in Business in each state
-    BUSINESS_MAJORS_BY_STATE = []
-    BUSINESS_MAJOR_STATES_LIST = list(BROADER_MAJOR_SUMMED_DATA_DF.loc \
-        [BROADER_MAJOR_SUMMED_DATA_DF['Major'] == 'Business', 'State'])
+    business_majors_by_state = []
+    business_major_states_list = list(broader_major_summed_data_df.loc \
+        [broader_major_summed_data_df['Major'] == 'Business', 'State'])
 
-        
+
     # Reassign a state's name to 0 if there are no business majors in the top 10%
     # of schools in the state
-    i = 0
+    state_index = 0
 
-    while i < len(RM_STATES_LIST):
-        if BUSINESS_MAJOR_STATES_LIST.count(RM_STATES_LIST[i]) == 0:
-            RM_STATES_LIST[i] = 0
-            i += 1
+    while state_index < len(rm_states_list):
+        if business_major_states_list.count(rm_states_list[state_index]) == 0:
+            rm_states_list[state_index] = 0
+            state_index += 1
         else:
-            i += 1
+            state_index += 1
 
 
     # Reassign state names with the number of students majoring in business in the
     # respective state
-    for current_state in RM_STATES_LIST:
+    for current_state in rm_states_list:
         if current_state != 0:
-            BUSINESS_MAJORS_BY_STATE.append(int(BROADER_MAJOR_SUMMED_DATA_DF.loc \
-                [(BROADER_MAJOR_SUMMED_DATA_DF['Major'] == "Business") & \
-                    (BROADER_MAJOR_SUMMED_DATA_DF['State'] == current_state), \
+            business_majors_by_state.append(int(broader_major_summed_data_df.loc \
+                [(broader_major_summed_data_df['Major'] == "Business") & \
+                    (broader_major_summed_data_df['State'] == current_state), \
                         'Students']))
         else:
-            BUSINESS_MAJORS_BY_STATE.append(0)
+            business_majors_by_state.append(0)
 
     # Convert the number of students majoring in business per state to a ratio
     # (business majors/all majors)
-    BUSINESS_MAJORS_BY_STATE_PERCENT = [i / j for i, j in zip \
-        (BUSINESS_MAJORS_BY_STATE, ALL_STUDENTS_PER_STATE)]
+    business_majors_by_state_percent = [i / j for i, j in zip \
+        (business_majors_by_state, ALL_STUDENTS_PER_STATE)]
 
     # Add a column to the dataframe containing business major ratio data
-    STATES = STATES.assign(Business_Majors=BUSINESS_MAJORS_BY_STATE_PERCENT)
+    states = states.assign(Business_Majors=business_majors_by_state_percent)
 
-    # Set the coordinate reference system for the STATES shapefile
-    STATES.crs = "EPSG:3395"
+    # Set the coordinate reference system for the states shapefile
+    states.crs = "EPSG:3395"
 
     # Plot and Save USA Map & Generate scale
-    FIG, AX = plt.subplots(1, 1)
-    STATES.plot(column='Business_Majors', ax=AX, legend=True)
-    plt.show()
+    states.plot(column='Business_Majors', legend=True)
+    plt.title('Density of Business Majors in Each State')
     plt.savefig("business_density_map.png")
+    plt.show()
 
-def generate_engineering_density_map(BROADER_MAJOR_SUMMED_DATA_DF, RM_STATES_LIST, STATES):
+def generate_engineering_density_map(broader_major_summed_data_df, rm_states_list, states):
+    """
+    This function creates a map of the USA, representing the density of
+    engineering majors in each state.
 
+    Args:
+        broader_major_summed_data_df: A pandas dataframe which represents the
+        number of students in all majors for all states.
+        rm_states_list:  A list of all states in the USA except for Alaska and Hawaii.
+        states: A geopandas dataframe containing the shape data for the USA plot.
+    """
     # Create a list of the number of students majoring in Engineering in each state
-    ENGINEERING_MAJORS_BY_STATE = []
-    ENGINEERING_MAJOR_STATES_LIST = list(BROADER_MAJOR_SUMMED_DATA_DF.loc \
-        [BROADER_MAJOR_SUMMED_DATA_DF['Major'] == 'Engineering', 'State'])
+    engineering_majors_by_state = []
+    engineering_major_states_list = list(broader_major_summed_data_df.loc \
+        [broader_major_summed_data_df['Major'] == 'Engineering', 'State'])
 
     # Reassign a state's name to 0 if there are no engineering majors in the
     # top 10% of schools in the state
-    i = 0
+    state_index = 0
 
-    while i < len(RM_STATES_LIST):
-        if ENGINEERING_MAJOR_STATES_LIST.count(RM_STATES_LIST[i]) == 0:
-            RM_STATES_LIST[i] = 0
-            i += 1
+    while state_index < len(rm_states_list):
+        if engineering_major_states_list.count(rm_states_list[state_index]) == 0:
+            rm_states_list[state_index] = 0
+            state_index += 1
         else:
-            i += 1
+            state_index += 1
 
     # Reassign state names with the number of students majoring in engineering
     # in the respective state
-    for current_state in RM_STATES_LIST:
+    for current_state in rm_states_list:
         if current_state != 0:
-            ENGINEERING_MAJORS_BY_STATE.append(int\
-                (BROADER_MAJOR_SUMMED_DATA_DF.loc[(BROADER_MAJOR_SUMMED_DATA_DF \
-                    ['Major'] == "Engineering") & (BROADER_MAJOR_SUMMED_DATA_DF \
+            engineering_majors_by_state.append(int\
+                (broader_major_summed_data_df.loc[(broader_major_summed_data_df \
+                    ['Major'] == "Engineering") & (broader_major_summed_data_df \
                         ['State'] == current_state), 'Students']))
         else:
-            ENGINEERING_MAJORS_BY_STATE.append(0)
+            engineering_majors_by_state.append(0)
 
     # Convert the number of students majoring in engineering per state to a ratio
     # (business majors/all majors)
-    ENGINEERING_MAJORS_BY_STATE_PERCENT = [i / j for i, j in zip \
-        (ENGINEERING_MAJORS_BY_STATE, ALL_STUDENTS_PER_STATE)]
+    engineering_majors_by_state_percent = [i / j for i, j in zip \
+        (engineering_majors_by_state, ALL_STUDENTS_PER_STATE)]
 
     # Add a column to the dataframe containing engineering major ratio data
-    STATES = STATES.assign(Engineering_Majors=ENGINEERING_MAJORS_BY_STATE_PERCENT)
+    states = states.assign(Engineering_Majors=engineering_majors_by_state_percent)
 
-    # Set the coordinate reference system for the STATES shapefile
-    STATES.crs = "EPSG:3395"
+    # Set the coordinate reference system for the states shapefile
+    states.crs = "EPSG:3395"
 
     # Plot and Save USA Map & Generate scale
-    FIG, AX = plt.subplots(1, 1)
-    STATES.plot(column='Engineering_Majors', ax=AX, legend=True)
-    plt.show()
+    states.plot(column='Engineering_Majors', legend=True)
+    plt.title('Density of Engineering Majors in Each State')
     plt.savefig("engineering_density_map.png")
+    plt.show()
 
+# Generate all Plots
 plot_number_of_students_per_major_usa()
 plot_major_ratios_in_stacked_bar_graph()
 plot_major_distribution_in_usa_pie_chart()
